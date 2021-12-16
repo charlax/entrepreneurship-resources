ff2eef7619b4cc01f25b11f8778d17a1fab44f10
Charles-Axel Dein <120501+charlax@users.noreply.github.com>
2021-12-16 09:45:20 +0100

    Improve pre-commit config

---
+++ .markdownlint-cli2.js
@@ -0,0 +1,16 @@
+// @ts-check
+
+"use strict";
+
+module.exports = {
+  config: {
+    "no-multiple-blanks": false,
+    "no-bare-urls": false,
+    "line-length": false,
+    "no-missing-space-atx": false,
+    "no-alt-text": false,
+    "no-trailing-punctuation": false,
+    "no-inline-html": false,
+    "no-duplicate-heading": false,
+  },
+};
